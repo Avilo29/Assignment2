1aefca9ff294100ce1e96d473002ceb54e8f091f
Avilo29 <olivas22@rowan.edu>
2024-10-04 11:48:55 -0400

    Update Task_1.m

--- Task_1.m
+++ Task_1.m
@@ -1,5 +1,10 @@
 % Author Name: Sydnie Oliva
 % Email: Olivas22@rowan.edu
 % Course: MATLAB Programming - Fall 2024
-% Assignment: Homework 1
-% Date: 9/20/24
+% Assignment: Population Growth Analysis
+% Date: 10//24
+
+population = [5000 5200 5350 5600 5800];
+growth_rate = [0 200 150 250 200];
+pop_data = [population growth_rate];
+fprintf(pop_data)
